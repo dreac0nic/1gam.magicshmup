5fcd2e0b377e56a1ae98d224ad2fb21931e579bc
SpenSer Bray <matthewqa@gmail.com>
2016-01-10 03:28:57 -0800

    Add some camera look around to make movement not awful.

---
+++ Assets/scripts/utility/LookTowardsTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookTowardsTarget : MonoBehaviour
+{
+	public Transform Target;
+	public float Speed = 5.0f;
+
+	public void Update()
+	{
+		if(Target) {
+			Quaternion new_rotation = Quaternion.LookRotation(Target.position - transform.position);
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, new_rotation, Speed*Time.deltaTime);
+		}
+	}
+}
