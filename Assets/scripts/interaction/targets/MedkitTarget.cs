51192af94d3f4476a5e0cf11a6ce0cddc6412f76
SpenSer Bray <matthewqa@gmail.com>
2016-01-12 17:42:02 -0800

    Create a Medkit interaction target. It's super dirty.

---
+++ Assets/scripts/interaction/targets/MedkitTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MedkitTarget : FireTargetBase
+{
+	public float HealthStrength = 15.0f;
+
+	public override void Fire(GameObject offender, TriggerType type = TriggerType.GENERAL)
+	{
+		Living living_entity = offender.GetComponentInChildren<Living>();
+
+		if(living_entity.IsAlive) {
+			living_entity.ApplyDamage(-1*HealthStrength);
+			Destroy(this);
+		}
+	}
+}
