ba891dba0bc6625dec38acd12f8b6ea230344286
SpenSer Bray <matthewqa@gmail.com>
2016-01-12 17:47:18 -0800

    Expand 'MaxHealth' name.

--- Assets/scripts/mechanics/Living.cs
+++ Assets/scripts/mechanics/Living.cs
@@ -5,7 +5,7 @@ public class Living : MonoBehaviour
 {
 
 	[SerializeField] protected float m_Health = 100.0f;
-	public float MaxHealth = 100.0f;
+	public float MaximumHealth = 100.0f;
 	public float HealthCooldownSpeed = 1.0f;
 
 	public bool IsAlive { get { return m_Health > 0.0f; } }
@@ -13,8 +13,8 @@ public class Living : MonoBehaviour
 
 	public void Update()
 	{
-		if(m_Health > MaxHealth) {
-			m_Health -= Mathf.Clamp(m_Health - MaxHealth, 0.0f, HealthCooldownSpeed);
+		if(m_Health > MaximumHealth) {
+			m_Health -= Mathf.Clamp(m_Health - MaximumHealth, 0.0f, HealthCooldownSpeed);
 		}
 	}
 
